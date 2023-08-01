a24118ecac4000d813190a414610ce08ade212e9
Anna S. A <anna.atalay@minna.tech>
2023-08-01 22:42:03 +0200

    Lesson 21 completed

--- Lesson21/app.js
+++ Lesson21/app.js
@@ -1,2 +1,34 @@
+const textInput = document.getElementById('todo-input');
+const form = document.getElementById('todo-form');
+const list = document.getElementById('todo-list');
 
-  
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	if (textInput.value == '') {
+		alert('Provide valid input');
+	} else {
+		const newItem = document.createElement('li');
+		const forText = document.createElement('span');
+		forText.innerText = textInput.value;
+
+		newItem.addEventListener('click', function () {
+			forText.classList.toggle('completed');
+		});
+
+		const deleteButton = document.createElement('button');
+		deleteButton.innerText = 'x';
+    console.log(deleteButton);
+    deleteButton.classList.add('button--light');
+        // deleteButton.setAttribute("class","button--light");
+		deleteButton.addEventListener('click', function () {
+			newItem.remove();
+		});
+
+		newItem.appendChild(forText);
+		newItem.appendChild(deleteButton);
+		list.appendChild(newItem);
+
+		textInput.value = '';
+	}
+});

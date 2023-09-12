e6d5dd893fb59ab5385db0047dd7bcb2ca60f470
Anna S. A <anna.atalay@minna.tech>
2023-09-12 18:15:35 +0200

    Add AJAX example

---
+++ Lesson25/app/script.js
@@ -0,0 +1,17 @@
+const configurations = {
+  api_key: 'qCnxm3oi9NS9mzAkGmh7_4B6V3p6DJnyFs_wKfNQFF4',
+  api_base_url: 'https://api.unsplash.com/photos/random',
+  number_of_images_to_fetch: 10,
+};
+
+// Parameters to use: client_id, count
+const image_url = '';
+
+// Function to fetch images from Unsplash
+function fetchImages() {
+  // Clear previous images
+}
+
+// Fetch images once button is clicked. 
+// Fetch images once page is loaded
+

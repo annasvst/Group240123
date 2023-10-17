65ee08adf0620b641c5f007a107ec7fa62241399
Anna S. A <anna.atalay@minna.tech>
2023-10-17 21:05:34 +0200

    Add lesson29 completed files

--- Lesson29/my-app/src/App.jsx
+++ Lesson29/my-app/src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
+import {List} from './components/List'
 
 export const App = () => {
+  // const paragraph = (
+  //   <p>Hello!</p>
+  // );
 
   return (
     <div className="app">
       <header className="app-header">
         To-do list
       </header>
+      {/*
+      - Add a TodoList component
+      - TodoList component would contain TodoListItem component
+      */}
+      <List />
     </div>
   );
 };
--- Lesson29/my-app/src/components/ListItem/index.jsx
+++ Lesson29/my-app/src/components/ListItem/index.jsx
@@ -1,2 +1,19 @@
 import './styles.css';
 
+// export const ListItem = (props) => <li>{title}</li>;
+
+
+export const ListItem = ({title}) => <li>{title}</li>;
+
+// JS object destructuring (ES6)
+const myObject = {
+  name: 'Anna',
+  surname: 'Atalay',
+  age: 30,
+};
+
+console.log(myObject.name, myObject.surname, myObject.age);
+
+const {name: myName, surname, age} = myObject;
+console.log(myName, surname, age);
+

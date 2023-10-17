e11dcb8beae09a4ac17fe192a161efafe6c0df6b
Anna S. A <anna.atalay@minna.tech>
2023-10-17 19:13:43 +0200

    Add lesson29 starter files

---
+++ Lesson29/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+export const App = () => {
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        To-do list
+      </header>
+    </div>
+  );
+};
---
+++ Lesson29/my-app/src/components/List/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+const data = [
+  {id: 1, title: "Do dishes"},
+  {id: 2, title: "Do homework"},
+  {id: 3, title: "Go running"},
+  {id: 4, title: "Do dishes 2"},
+  {id: 5, title: "Do homework 3"},
+  {id: 6, title: "Go running 4"},
+];

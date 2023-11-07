981f56cc5ea4dd0b109ecc3e355ebfa55d38fbd0
Anna S. A <anna.atalay@minna.tech>
2023-11-07 16:09:36 +0100

    Add Lesson32 starter files

---
+++ Lesson32/my-app/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+export const App = () => {
+	return (
+		<div className='container'>
+			<header>Recipe Search App</header>
+		</div>
+	);
+};
---
+++ Lesson32/my-app/src/components/RecipeList/index.jsx
@@ -0,0 +1,8 @@
+export const RecipeList = () => {
+	return (
+		<div>
+			<h2>Search Results</h2>
+			<ul></ul>
+		</div>
+	);
+};
